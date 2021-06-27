382d857afa1d1733e1034859d9ca9c7fc5046d1f
her0807 <her0807@naver.com>
2021-06-27 15:47:59 +0900

    feat: 프로젝트 초기 설정
    Spring Web,Spring Data JPA, Spring Boot DevTools, Lombok 추가

---
+++ src/main/java/com/tistory/hellodevelop/nbread/NbreadApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.hellodevelop.nbread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbreadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbreadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tistory/hellodevelop/nbread/NbreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.hellodevelop.nbread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
